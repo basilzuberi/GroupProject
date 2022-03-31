083b7e80903e3ec037462283809c270fcc5e77b8
Abdul-Baseet Shabi <sbaseet123@gmail.com>
2022-03-30 22:50:40 -0400

    using relative path, accounting for multiple matches

--- zube5220_q1/listallthethings.sh
+++ zube5220_q1/listallthethings.sh
@@ -1,30 +1,33 @@
 files=($@)
 file_paths=()
 
-#getting all the files in the directory and subdirectories as their absolute paths
+#getting all the files in the directory and subdirectories as their relative paths
 getAllFilesPaths() {
-    for item in $1/*
+    #getting the content of the directory
+    long_list=($(ls $1))
+    for item in ${long_list[@]}
     do
-        if [[ -d $item ]]           # If directory
+        item_path="$1$item"
+        if [[ -d $item_path ]]           # If directory
         then
-            getAllFilesPaths $item
-        elif [[ -f $item ]]           # If file
+            #recursion
+            getAllFilesPaths "$item_path/"
+        elif [[ -f $item_path ]]           # If file
         then
-            file_paths+=( $item )
+            file_paths+=( "$item_path" )
         fi
     done
 }
-#iterating through all the absolute paths of files only and printing the long list for the path that has the filename appended to it i.e. /{filename}
+#iterating through all the relative paths of "files only" and printing the long list for the path that has the filename appended to it (if nested i.e. parent/{filename}) or filename only (i.e. {filname})
 printFileLongList(){
     found=0
     i=0
-    num_of_file_path=${#file_paths[@]} #https://www.cyberciti.biz/faq/finding-bash-shell-array-length-elements/
-    while [ "$found" -eq "0" ] && [ "$i" -lt "$num_of_file_path" ]
+    for path in ${file_paths[@]}
     do
-        find_file=$(echo ${file_paths[$i]} | grep -E "(\/$1)$" )
+        find_file=$(echo ${path} | grep -E "(^($1)$)|((\/$1)$)" )
         if [ "$find_file" != "" ]
         then
-            response=$(ls -l "${file_paths[$i]}")
+            response=$(ls -l "${path}")
             echo $response
             found=1
         fi
@@ -37,7 +40,7 @@ printFileLongList(){
     fi
 }
 
-getAllFilesPaths $PWD
+getAllFilesPaths ""
 
 #going through the file provided by the user
 for file in ${files[@]}
