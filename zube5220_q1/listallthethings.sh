ad56fbc4b02e0927ba6ee362e7000543accb25e6
Abdul-Baseet Shabi <shab7430@mylaurier.ca>
2022-03-30 20:33:51 -0400

    Final Implemenatation for Q1 without b or c

--- zube5220_q1/listallthethings.sh
+++ zube5220_q1/listallthethings.sh
@@ -1,22 +1,47 @@
-search() {
-    echo $1
+files=($@)
+file_paths=()
+
+getAllFilesPaths() {
     for item in $1/*
     do
         # echo "TEST $item" 
         if [[ -d $item ]]           # If directory
         then
-            search $item
-        fi
-
-        if [[ -f $item ]]           # If file
+            getAllFilesPaths $item
+        elif [[ -f $item ]]           # If file
         then
-            echo "l"
+            file_paths+=( $item )
         fi
     done
 }
 
+printFileLongList(){
+    found=0
+    i=0
+    num_of_file_path=${#file_paths[@]} #https://www.cyberciti.biz/faq/finding-bash-shell-array-length-elements/
+    while [ "$found" -eq "0" ] && [ "$i" -lt "$num_of_file_path" ]
+    do
+        find_file=$(echo ${file_paths[$i]} | grep -E "(\/$1)$" )
+        if [ "$find_file" != "" ]
+        then
+            response=$(ls -l "${file_paths[$i]}")
+            echo $response
+            found=1
+        fi
+        i=$(( $i + 1 ))
+    done
 
-echo "Doing a search for given files..."
-search $PWD
-echo "Finished Search."
+    if [ "$found" -eq "0" ] 
+    then 
+        echo "Filename ($1) does not exist in this directory or any subdirectories"
+    fi
+}
+
+getAllFilesPaths $PWD
+for file in ${files[@]}
+do
+    echo "ls -l for filename: $file"
+    printFileLongList $file
+    echo ""
+done
 exit 0
